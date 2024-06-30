4f544fc50367faa8393a6cbf6a1d2714d4dc1b71
expiratory <expiratory@yandex.ru>
2024-06-30 18:09:17 +0300

    hw_1 added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "2.5.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "org.example"
@@ -12,6 +14,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
 
 tasks.test {
